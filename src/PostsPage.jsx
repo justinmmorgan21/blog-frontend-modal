996bb9882c70bd917e2894e68b3247d066c5c11d
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-19 18:35:09 -0500

    add Modal css and jsx

--- src/PostsPage.jsx
+++ src/PostsPage.jsx
@@ -1,11 +1,13 @@
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Modal } from "./Modal";
 
 export function PostsPage() {
   // let posts = [];
   const [posts, setPosts] = useState([]);
+  
   const handleIndex = () => {
     axios.get("http://localhost:3000/posts.json").then(response => {
       // console.log(response.data);
@@ -13,11 +15,16 @@ export function PostsPage() {
     })
   }
 
+  useEffect(handleIndex, []);
+
   return (
     <main>
       <PostsNew />
       <PostsIndex posts={posts} />
-      <button onClick={handleIndex} >Get data</button>
+      {/* <button onClick={handleIndex} >Get data</button> */}
+      <Modal show={true}>
+        <p>TEST</p>
+      </Modal>
     </main>
   );
 }
